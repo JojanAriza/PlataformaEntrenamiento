f08ec3af0975940b9eb81a18306fabbbd31c51b1
sankiago <lpzsds@gmail.com>
2025-03-21 11:43:33 -0500

    FIX: Restauración del archivo CourseList

---
+++ client/src/views/CourseList.tsx
@@ -0,0 +1,20 @@
+import { cursos } from "../data/cursos";
+import Pagination from "../components/Course/Pagination"
+import SearchBar from "../components/Course/SearchBar"
+import CourseRow from "../components/CourseRow";
+
+export default function CourseList() {
+    const handleSearch = (searchTerm:string) => {
+        console.log("Buscar: ", searchTerm);
+        // Aquí puedes implementar la lógica de búsqueda si lo necesitas
+      };
+    return (
+        <div className="py-6 px-10 space-y-2 w-full">
+            <SearchBar onSearch={handleSearch} />
+            {cursos.map((curso) => (
+                <CourseRow {...curso}></CourseRow>
+            ))}
+             <Pagination/>
+        </div>
+    );
+}
