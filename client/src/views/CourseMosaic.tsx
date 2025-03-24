087330298edbf5225497dd0852590b273a0d6821
sankiago <lpzsds@gmail.com>
2025-03-24 18:51:14 -0500

    FIX: Corrección de casos cercanos al media-querie breakpoint en el compoente CourseMosaic

--- client/src/views/CourseMosaic.tsx
+++ client/src/views/CourseMosaic.tsx
@@ -18,9 +18,8 @@ export default function CourseMosaic({ cursos }: MosaicParams) {
     );
 
   return (
-    <div className=" py-6 px-0 md:px-4 w-full flex justify-center">
-      {/* <div className="grid grid-cols-4 gap-6"> */}
-      <div className="grid grid-cols-2 gap-4 md:grid-cols-4 md:gap-6 justify-items-center w-full">
+    <div className="py-6 px-0 md:px-4 w-full flex justify-center">
+      <div className="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4 md:gap-6 justify-items-center w-full">
         {cursos.map((curso) => (
           <CourseCard {...curso}></CourseCard>
         ))}
